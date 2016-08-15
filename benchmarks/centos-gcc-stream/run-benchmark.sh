b9974b8c8b02db9cd082f45576e0bbce8cd90541
Nico Mittenzwey @ Home <nico.mittenzwey@gmail.com>
2016-08-15 22:45:50 +0200

    Initial Commit

---
+++ benchmarks/centos-gcc-stream/run-benchmark.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+MB=$(free | grep Mem | tr -s ' ' | cut -d ' ' -f 4);
+ARRAYSIZE=$(( $MB * 40 ));
+
+gcc -funsafe-math-optimizations -fprefetch-loop-arrays -ftree-vectorize -fstrict-aliasing -mcmodel=large -Ofast -mtune=native -march=native -fopenmp stream.c -DSTREAM_ARRAY_SIZE=$ARRAYSIZE -o stream-gcc
+
+CORESPERSOCKET=$(lscpu | grep "per socket" | tr -s ' ' | cut -d ' ' -f4);
+SOCKETS=$(lscpu | grep Socket | tr -s ' ' | cut -d ' ' -f2);
+
+OMP_NUM_THREADS=$(( $CORESPERSOCKET * $SOCKETS )) ./stream-gcc
