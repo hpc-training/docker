3e73e95cef80ecc0e8f60e02618aed3ece83b69d
Nico Mittenzwey <nico.mittenzwey@gmail.com>
2016-08-15 23:45:48 +0200

    fixed directories

--- benchmarks/centos-gcc-stream/run-benchmark.sh
+++ benchmarks/centos-gcc-stream/run-benchmark.sh
@@ -2,7 +2,7 @@
 MB=$(free | grep Mem | tr -s ' ' | cut -d ' ' -f 4);
 ARRAYSIZE=$(( $MB * 40 ));
 
-gcc -funsafe-math-optimizations -fprefetch-loop-arrays -ftree-vectorize -fstrict-aliasing -mcmodel=large -Ofast -mtune=native -march=native -fopenmp stream.c -DSTREAM_ARRAY_SIZE=$ARRAYSIZE -o stream-gcc
+gcc -funsafe-math-optimizations -fprefetch-loop-arrays -ftree-vectorize -fstrict-aliasing -mcmodel=large -Ofast -mtune=native -march=native -fopenmp /usr/src/stream/stream.c -DSTREAM_ARRAY_SIZE=$ARRAYSIZE -o stream-gcc
 
 CORESPERSOCKET=$(lscpu | grep "per socket" | tr -s ' ' | cut -d ' ' -f4);
 SOCKETS=$(lscpu | grep Socket | tr -s ' ' | cut -d ' ' -f2);
