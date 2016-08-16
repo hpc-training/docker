c7eb412e3db6622f46f064f79d80aabe9a071528
Nico Mittenzwey <nico.mittenzwey@gmail.com>
2016-08-16 22:33:20 +0200

    simplified compile flags and added GOMP_CPU_AFFINITY envrionment variable

--- benchmarks/centos-gcc-stream/run-benchmark.sh
+++ benchmarks/centos-gcc-stream/run-benchmark.sh
@@ -2,9 +2,11 @@
 MB=$(free | grep Mem | tr -s ' ' | cut -d ' ' -f 4);
 ARRAYSIZE=$(( $MB * 40 ));
 
-gcc -funsafe-math-optimizations -fprefetch-loop-arrays -ftree-vectorize -fstrict-aliasing -mcmodel=large -Ofast -mtune=native -march=native -fopenmp /usr/src/stream/stream.c -DSTREAM_ARRAY_SIZE=$ARRAYSIZE -o stream-gcc
+gcc -fstrict-aliasing -mcmodel=large -Ofast -mtune=native -march=native -fopenmp /usr/src/stream/stream.c -DSTREAM_ARRAY_SIZE=$ARRAYSIZE -o stream-gcc
 
 CORESPERSOCKET=$(lscpu | grep "per socket" | tr -s ' ' | cut -d ' ' -f4);
 SOCKETS=$(lscpu | grep Socket | tr -s ' ' | cut -d ' ' -f2);
 
-OMP_NUM_THREADS=$(( $CORESPERSOCKET * $SOCKETS )) ./stream-gcc
+export OMP_NUM_THREADS=$(( $CORESPERSOCKET * $SOCKETS ));
+export GOMP_CPU_AFFINITY="0-$(($OMP_NUM_THREADS - 1))"
+ ./stream-gcc
